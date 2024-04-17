ed663f4130ae28ebfb14233434ef107fe39ea1cf
shayanbalochofficial <144522960+shayanbalochofficial@users.noreply.github.com>
2024-04-17 23:46:59 +0500

    Initial commit

---
+++ FF.js
@@ -0,0 +1,6 @@
+"use strict";
+let favorite_fruits = ['Kiwi', 'Banana', 'Apple'];
+//* Using For of Loop
+for (let fruit of favorite_fruits) {
+    console.log(`You really like ${fruit} !`);
+}
---
+++ FF.ts
@@ -0,0 +1,8 @@
+let favorite_fruits = ['Kiwi', 'Banana', 'Apple'];
+
+//* Using For of Loop
+for (let fruit of favorite_fruits) {
+    console.log(`You really like ${fruit} !`);
+}
+
+
